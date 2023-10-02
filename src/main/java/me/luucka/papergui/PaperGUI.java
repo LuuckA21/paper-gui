65ee17eaa06b25cf95e3430ccd0afd675b1506ee
LuuckA <luca.dalu@outlook.it>
2023-10-02 23:35:54 +0200

    Make PaperGUI singleton class and some fix

--- src/main/java/me/luucka/papergui/PaperGUI.java
+++ src/main/java/me/luucka/papergui/PaperGUI.java
@@ -1,7 +1,5 @@
 package me.luucka.papergui;
 
-import lombok.Getter;
-import lombok.Setter;
 import me.luucka.papergui.buttons.PGButton;
 import me.luucka.papergui.item.UtilityItem;
 import me.luucka.papergui.menu.PGMenu;
@@ -17,61 +15,84 @@ import java.util.List;
 
 public final class PaperGUI {
 
+    private static PaperGUI instance;
+
     private final JavaPlugin plugin;
 
-    @Setter
     private String prevButtonName = "<green><bold>← Previous Page";
-    @Setter
-    private List<String> prevButtonLore = List.of("<green>Click to move back to", "<green>page {PAGE}.");
-
-    @Setter
+    private final List<String> prevButtonLore = new ArrayList<>() {{
+        add("<green>Click to move back to");
+        add("<green>page {PAGE}.");
+    }};
     private String currentButtonName = "<gray><bold>Page {PAGE} of {MAX_PAGE}";
-    @Setter
-    private List<String> currentButtonLore = List.of("<gray>You are currently viewing", "<gray>page {PAGE}.");
-
-    @Setter
+    private final List<String> currentButtonLore = new ArrayList<>() {{
+        add("<gray>You are currently viewing");
+        add("<gray>page {PAGE}.");
+    }};
     private String nextButtonName = "<green><bold>Next Page →";
-    @Setter
-    private List<String> nextButtonLore = List.of("<green>Click to move forward to", "<green>page {PAGE}.");
+    private final List<String> nextButtonLore = new ArrayList<>() {{
+        add("<green>Click to move forward to");
+        add("<green>page {PAGE}.");
+    }};
 
-    @Getter
-    @Setter
     private boolean blockDefaultInteractions = true;
-    @Getter
-    @Setter
     private boolean enableAutomaticPagination = true;
-    @Getter
-    @Setter
-    private PGPaginationButtonBuilder defaultPaginationButtonBuilder = (type, inventory) -> {
+
+    private final PGPaginationButtonBuilder defaultPaginationButtonBuilder = (type, inventory) -> {
         switch (type) {
             case PREV_BUTTON -> {
-                if (inventory.getCurrentPage() > 0) return new PGButton(new UtilityItem(Material.ARROW)
-                        .setDisplayName(MiniMessage.miniMessage().deserialize(prevButtonName.replace("{PAGE}", String.valueOf(inventory.getCurrentPage()))))
-                        .setLore(convertLore(prevButtonLore, inventory.getCurrentPage()))
-                        .toItemStack()
-                ).withListener(event -> {
-                    event.setCancelled(true);
-                    inventory.previousPage(event.getWhoClicked());
-                });
-                else return null;
+                if (inventory.getCurrentPage() > 0) {
+                    return new PGButton(new UtilityItem(Material.ARROW)
+                            .setDisplayName(MiniMessage.miniMessage().deserialize(
+                                    prevButtonName.replace(
+                                            "{PAGE}",
+                                            String.valueOf(inventory.getCurrentPage())
+                                    )
+                            ))
+                            .setLore(convertLore(prevButtonLore, inventory.getCurrentPage()))
+                            .build()
+                    ).withListener(event -> {
+                        event.setCancelled(true);
+                        inventory.previousPage(event.getWhoClicked());
+                    });
+                } else {
+                    return null;
+                }
             }
             case CURRENT_BUTTON -> {
                 return new PGButton(new UtilityItem(Material.NAME_TAG)
-                        .setDisplayName(MiniMessage.miniMessage().deserialize(currentButtonName.replace("{PAGE}", String.valueOf(inventory.getCurrentPage() + 1)).replace("{MAX_PAGE}", String.valueOf(inventory.getMaxPage()))))
+                        .setDisplayName(MiniMessage.miniMessage().deserialize(
+                                currentButtonName
+                                        .replace(
+                                                "{PAGE}",
+                                                String.valueOf(inventory.getCurrentPage() + 1)
+                                        )
+                                        .replace(
+                                                "{MAX_PAGE}",
+                                                String.valueOf(inventory.getMaxPage())
+                                        )
+                        ))
                         .setLore(convertLore(currentButtonLore, inventory.getCurrentPage() + 1))
-                        .toItemStack()
+                        .build()
                 ).withListener(event -> event.setCancelled(true));
             }
             case NEXT_BUTTON -> {
-                if (inventory.getCurrentPage() < inventory.getMaxPage() - 1)
+                if (inventory.getCurrentPage() < inventory.getMaxPage() - 1) {
                     return new PGButton(new UtilityItem(Material.ARROW)
-                            .setDisplayName(MiniMessage.miniMessage().deserialize(nextButtonName.replace("{PAGE}", String.valueOf(inventory.getCurrentPage() + 2))))
-                            .setLore(convertLore(nextButtonLore, inventory.getCurrentPage() + 2)).toItemStack()
+                            .setDisplayName(MiniMessage.miniMessage().deserialize(
+                                    nextButtonName.replace(
+                                            "{PAGE}",
+                                            String.valueOf(inventory.getCurrentPage() + 2)
+                                    )
+                            ))
+                            .setLore(convertLore(nextButtonLore, inventory.getCurrentPage() + 2)).build()
                     ).withListener(event -> {
                         event.setCancelled(true);
                         inventory.nextPage(event.getWhoClicked());
                     });
-                else return null;
+                } else {
+                    return null;
+                }
             }
             default -> {
                 return null;
@@ -79,12 +100,74 @@ public final class PaperGUI {
         }
     };
 
-    public PaperGUI(JavaPlugin plugin) {
+    private PaperGUI(JavaPlugin plugin) {
         this.plugin = plugin;
 
         plugin.getServer().getPluginManager().registerEvents(
                 new PGMenuListener(plugin, this), plugin
         );
+    }
+
+    public static PaperGUI getInstance(JavaPlugin plugin) {
+        if (instance == null) {
+            instance = new PaperGUI(plugin);
+        }
+        return instance;
+    }
+
+    public PaperGUI prevButtonName(String prevButtonName) {
+        this.prevButtonName = prevButtonName;
+        return this;
+    }
+
+    public PaperGUI prevButtonLore(List<String> prevButtonLore) {
+        this.prevButtonLore.clear();
+        this.prevButtonLore.addAll(prevButtonLore);
+        return this;
+    }
+
+    public PaperGUI currentButtonName(String currentButtonName) {
+        this.currentButtonName = currentButtonName;
+        return this;
+    }
+
+    public PaperGUI currentButtonLore(List<String> currentButtonLore) {
+        this.currentButtonLore.clear();
+        this.currentButtonLore.addAll(currentButtonLore);
+        return this;
+    }
+
+    public PaperGUI nextButtonName(String nextButtonName) {
+        this.nextButtonName = nextButtonName;
+        return this;
+    }
+
+    public PaperGUI nextButtonLore(List<String> nextButtonLore) {
+        this.nextButtonLore.clear();
+        this.nextButtonLore.addAll(nextButtonLore);
+        return this;
+    }
+
+    public boolean isBlockDefaultInteractions() {
+        return blockDefaultInteractions;
+    }
+
+    public PaperGUI blockDefaultInteractions(boolean blockDefaultInteractions) {
+        this.blockDefaultInteractions = blockDefaultInteractions;
+        return this;
+    }
+
+    public boolean isEnableAutomaticPagination() {
+        return enableAutomaticPagination;
+    }
+
+    public PaperGUI enableAutomaticPagination(boolean enableAutomaticPagination) {
+        this.enableAutomaticPagination = enableAutomaticPagination;
+        return this;
+    }
+
+    public PGPaginationButtonBuilder getDefaultPaginationButtonBuilder() {
+        return defaultPaginationButtonBuilder;
     }
 
     public PGMenu create(String name, int rows) {
